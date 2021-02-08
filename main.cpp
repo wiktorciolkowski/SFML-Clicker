5fbde274abef87de7bb2959843d05e77965fa34b
Wiktor <wikcioo@gmail.com>
2021-02-08 18:10:24 +0100

    created sfml template

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <SFML/Graphics.hpp>
+#define RATIO (4.0/3.0)
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    const int WIDTH = 800;
+    sf::RenderWindow window(sf::VideoMode(WIDTH, WIDTH / RATIO), "Clicker", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event event{};
 
     while (window.isOpen())
     {
-        sf::Event event{};
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape)
+                    {
+                        window.close();
+                    }
+                    break;
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::Magenta);
         window.display();
     }
 
